9544341b28420c79a1bd0e985b7a2dfbefb1570a
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 21:50:23 -0800

    Add files via upload

---
+++ Q21.ts
@@ -0,0 +1,5 @@
+// Q21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person: {name:"sadaf", fname:string, age:number} = {name: "sadaf", fname: "sohail",age: 25}
+
+console.log(person)
